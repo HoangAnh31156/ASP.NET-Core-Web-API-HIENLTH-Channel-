0a96aa50f447f11d4ce2c4e32e11dc3bae467d99
Hoang Viet Anh <anhhvph31156@fpt.edu.vn>
2023-12-23 17:06:39 +0700

    Tạo API đầu tiên

---
+++ MyFirstWebAPI/MyFirstWebAPI/Controllers/HangHoaController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebAPI.Models;
+
+namespace MyFirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hangHoa);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }          
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hanghoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+
+            hangHoas.Add(hanghoa);
+            return Ok(new
+            {
+                Success = true, Data = hanghoa
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, HangHoa hangHoaUpdate)
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+
+                if(id != hangHoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                //Update
+                hangHoa.TenHangHoa = hangHoaUpdate.TenHangHoa;
+                hangHoa.DonGia = hangHoaUpdate.DonGia;
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(string id)
+        {
+            try
+            {
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                    return NotFound();
+
+                //Remove
+                hangHoas.Remove(hangHoa);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
